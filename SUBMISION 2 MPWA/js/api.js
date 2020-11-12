63d9b27c1edf2343414045746760b9f072ef733a
Mr_AzuarCod <azuarok@gmail.com>
2020-11-13 02:08:54 +0800

    Update Data

--- SUBMISION 2 MPWA/js/api.js
+++ SUBMISION 2 MPWA/js/api.js
@@ -183,9 +183,47 @@ function getTeamById() {
 }
 
 function showDetail(teamsdata) {
+    let teams = "";
+    let teamsDetail = document.getElementById("clubs-detail");
+
+    teamsdata.teams.forEach(function (club) {
+        teams += `
+        <tr>
+            <td>${club.id}</td>
+            <td>${club.name}</td>
+            <td>${club.address}</td>
+            <td>${club.phone}</td>
+            <td>${club.website}</td>
+            <td>${club.email}</td>
+            <td>${club.clubColors}</td>
+        </tr>
+        `
+    });
+    teamsDetail.innerHTML = `
+                <div class="card" style="padding-left: 24px; padding-right: 24px; margin-top: 30px;">
+
+                <table class="striped responsive-table">
+                    <thead>
+                        <tr>
+                            <th>Club ID</th>
+                            <th>Club Name</th>
+                            <th>Address</th>
+                            <th>Phone</th>
+                            <th>Website</th>
+                            <th>Email</th>
+                            <th>Club Color</th>
+                        </tr>
+                     </thead>
+                    <tbody id="standings">
+                        ${teams}
+                    </tbody>
+                </table>
+
+                </div>
+    `;
+
     let squad = "";
     let teamsElement = document.getElementById("list-player");
-
     teamsdata.squad.forEach(function (data) {
         squad += `
                 <tr>
@@ -219,136 +257,3 @@ function showDetail(teamsdata) {
                 </div>
     `;
 }
-
-
-// NO THINGS ABOUT IT
-// function getTeamById() {
-//     const urlParams = new URLSearchParams(window.location.search);
-//     const idParam = urlParams.get("id");
-
-//     let squadsHTML = "";
-
-//     if ("caches" in window) {
-//         caches
-//             .match(TEAMS_DETAIL + "/" + idParam)
-//             .then(function (response) {
-//                 if (response) {
-//                     response.json().then(function (data) {
-//                         data = JSON.parse(
-//                             JSON.stringify(data).replace(/http:/g, "https:")
-//                         );
-//                         // Memasukkan data pada element HTML berdasarkan ID
-//                         document.getElementById("club_logo").src = data.crestUrl;
-//                         document.getElementById("name").innerHTML = data.name;
-//                         document.getElementById("shortName").innerHTML = data.shortName;
-//                         document.getElementById("address").innerHTML = data.address;
-//                         document.getElementById("phone").innerHTML = data.phone;
-//                         document.getElementById("website").innerHTML = data.website;
-//                         document.getElementById("email").innerHTML = data.email;
-//                         document.getElementById("clubColors").innerHTML = data.clubColors;
-
-//                         data.squad.forEach(function (squad) {
-//                             // Menyusun komponen card artikel secara dinamis
-//                             squadsHTML += `
-//                             <div class="card z-depth-2">
-//                                 <div class="card-content">
-//                                 <h5>Player Info</h5>
-//                                         <table>
-//                                             <tbody>
-//                                                 <tr>
-//                                                     <td>ID:</td>
-//                                                     <td id="id">${squad.id}</td>
-//                                                 </tr>
-//                                                 <tr>
-//                                                     <td>Name:</td>
-//                                                     <td id="plyName">${squad.name}</td>
-//                                                 </tr>
-//                                                 <tr>
-//                                                     <td>Date Of Birth:</td>
-//                                                     <td id="dateOfBirth">${squad.dateOfBirth}</td>
-//                                                 </tr>
-//                                                 <tr>
-//                                                     <td>Nationality:</td>
-//                                                     <td id="nationality">${squad.nationality}</td>
-//                                                 </tr>
-//                                                 <tr>
-//                                                     <td>Position:</td>
-//                                                     <td id="position">${squad.position}</td>
-//                                                 </tr>
-//                                             </tbody>
-//                                         </table>
-//                                 </div>
-//                             </div>
-//                             `;
-//                         });
-
-//                         // Sisipkan komponen card ke dalam elemen dengan id #content
-//                         document.getElementById("list-player").innerHTML = squadsHTML;
-
-//                         // kirim object hasil parsing json agar bisa disimpan ke indexed db
-//                         resolve(data);
-//                     });
-//                 }
-//             });
-//     }
-
-//     fetchApi(TEAMS_DETAIL + "/" + idParam)
-//         .then(status)
-//         .then(json)
-//         .then(function (data) {
-//             data = JSON.parse(
-//                 JSON.stringify(data).replace(/http:/g, "https:")
-//             );
-//             // Memasukkan data pada element HTML berdasarkan ID
-//             document.getElementById("club_logo").src = data.crestUrl;
-//             document.getElementById("name").innerHTML = data.name;
-//             document.getElementById("shortName").innerHTML = data.shortName;
-//             document.getElementById("address").innerHTML = data.address;
-//             document.getElementById("phone").innerHTML = data.phone;
-//             document.getElementById("website").innerHTML = data.website;
-//             document.getElementById("email").innerHTML = data.email;
-//             document.getElementById("clubColors").innerHTML = data.clubColors;
-
-//             data.squad.forEach(function (squad) {
-//                 // Menyusun komponen card artikel secara dinamis
-//                 squadsHTML += `
-//                 <div class="card z-depth-2">
-//                     <div class="card-content">
-//                     <h5>Player Info</h5>
-//                             <table>
-//                                 <tbody>
-//                                     <tr>
-//                                         <td>ID:</td>
-//                                         <td id="id">${squad.id}</td>
-//                                     </tr>
-//                                     <tr>
-//                                         <td>Name:</td>
-//                                         <td id="plyName">${squad.name}</td>
-//                                     </tr>
-//                                     <tr>
-//                                         <td>Date Of Birth:</td>
-//                                         <td id="dateOfBirth">${squad.dateOfBirth}</td>
-//                                     </tr>
-//                                     <tr>
-//                                         <td>Nationality:</td>
-//                                         <td id="nationality">${squad.nationality}</td>
-//                                     </tr>
-//                                     <tr>
-//                                         <td>Position:</td>
-//                                         <td id="position">${squad.position}</td>
-//                                     </tr>
-//                                 </tbody>
-//                             </table>
-//                     </div>
-//                 </div>
-//                 `;
-//             });
-
-//             // Sisipkan komponen card ke dalam elemen dengan id #content
-//             document.getElementById("list-player").innerHTML = squadsHTML;
-
-//             // kirim object hasil parsing json agar bisa disimpan ke indexed db
-//             resolve(data);
-//         })
-//         .catch(error);
-// }
